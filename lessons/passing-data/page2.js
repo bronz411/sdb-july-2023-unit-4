cf5c1911515516c982821ed75edc78fb27bc8d16
Brent Brown <bronz411@gmail.com>
2023-08-02 12:57:46 -0600

    finished lecture for the day

--- lessons/passing-data/page2.js
+++ lessons/passing-data/page2.js
@@ -4,3 +4,11 @@ let queryString = window.location.search;
 console.log(queryString);
 
 let urlParams = new URLSearchParams(queryString);
+
+console.log(urlParams.get("product"));
+console.log(urlParams.get("quantity"));
+
+let orderElement = document.getElementById("order");
+let qty = urlParams.get("quantity");
+let product = urlParams.get("product");
+orderElement.textContent = `Your order you placed was ${qty} ${product}`;
